2b8c7fdfedfa4b023fa57efc145fc1ce9c8406bf
Ryanifar <ryanifar1414@gmail.com>
2022-06-06 22:31:14 +0800

    edit view create kesehatan

--- resources/views/kesehatan/create.blade.php
+++ resources/views/kesehatan/create.blade.php
@@ -42,7 +42,7 @@
 
     <div class="container">
     <div class="row">
-    <h1>Ini buat kesehatan</h1>
+    <h1>Ini nambah kesehatan</h1>
 
     <form method="post" action="{{route('store-kesehatan')}}">
         @csrf
