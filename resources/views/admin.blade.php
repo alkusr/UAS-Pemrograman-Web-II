171546dad8c62e4873eeae107a86c1003e379c91
Ryanifar <ryanifar1414@gmail.com>
2022-06-06 21:35:01 +0800

    edit admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
-
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">KEBUN BINATANG</a>
@@ -39,7 +38,6 @@
         </div>
     </div>
 </nav>
-
 <div class="container">
     <div class="row">
     <h1>Ini Web Zoo</h1>
@@ -55,7 +53,6 @@
     <a href="{{route('zoo-about')}}" class="btn btn-secondary" >Tentang Kami</a>
     <a href="{{route('welcome')}}" class="btn btn-secondary" >Kembali Kehalaman Utama</a>
     </div>
-    
     </div>
     </div>
 </body>
