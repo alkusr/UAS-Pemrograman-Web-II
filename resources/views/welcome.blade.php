ae15f5334e438a2f8be918ac31eee10b8a840b0a
danyadr4 <danyadrian47@gmail.com>
2022-06-07 07:49:07 +0800

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">KEBUN BINATANG</a>
+        <a class="navbar-brand" href="#">KEBUN BINATANG </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
